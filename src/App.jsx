a95a0a52d6e0ba90fbd1e89ae77c69722031b1d8
backiev <farkhat.bakiev@mail.ru>
2023-05-16 05:42:39 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState } from 'react'
+import './App.css'
+
+export const  App = () => {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='text-3xl font-bold underline'>
+        <div>1</div>
+        <div>2</div>
+      </div>
+    </>
+  )
+}
+
+export default App
